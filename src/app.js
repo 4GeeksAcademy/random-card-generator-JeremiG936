b4e00bc4caa4221cde6e01ebbb143f1a801b0d9c
JeremiG936 <jeremigonzalez1@gmail.com>
2025-04-27 17:15:26 +0000

    Se realizo random card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+let numbers = ["1", "2", "3", "4", "5", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let suits = ["♥", "♦", "♣", "♠"];
 
+function getRandom (arr) {
+    let random = Math.floor(Math.random() * arr.length);
+    return arr[random];
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let value = getRandom(suits);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let first = document.querySelector("#card-suit-1");
+first.innerHTML = value;
+
+let second = document.querySelector("#card-suit-2");
+second.innerHTML = value;
+
+let third = document.querySelector("#card-number");
+third.innerHTML = getRandom(numbers)
+
+if (value === "♥" || value === "♦") {
+    first.style.color = "red";
+    second.style.color = "red";
+} else {
+    first.style.color = "black";
+    second.style.color = "black";
+}
+
